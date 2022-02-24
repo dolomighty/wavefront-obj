53698e28bcc7455f1c48491da740f3bf9d635037
00519575 <andrealorenzobruno.villa@telecomitalia.it>
2022-02-24 17:59:32 +0100

    .

--- main.cpp
+++ main.cpp
@@ -1,29 +1,60 @@
 
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
 #include <vector>
 #include <string>
 #include <iostream>
+#include <iomanip>
 #include <unordered_map>
 
-#include <string.h>
-
-#include <assert.h>
 
 
 // importer per wavefront obj
 
 struct RGB {
     float r,g,b;
-} RGB;
+};
+
+struct XYZ {
+    float x,y,z;
+};
+
+
+
 
 struct MTL {
     struct RGB diff;
     struct RGB emit;
-} MTL;
+};
+
 
 std::unordered_map<std::string,struct MTL> mtl_list;
+// le liste di vertici/uv/ecc sono globali nel file
+// inoltre son 1-based, gestito
+std::vector<struct XYZ> vert_list;
+
+
+
+void out_xyz( struct XYZ &xyz ){
+    std::cout 
+    << "(" << std::fixed << std::setw(5) << std::setprecision(3) << std::setfill('0') << xyz.x
+    << " " << std::fixed << std::setw(5) << std::setprecision(3) << std::setfill('0') << xyz.y
+    << " " << std::fixed << std::setw(5) << std::setprecision(3) << std::setfill('0') << xyz.z
+    << ") ";
+}
+
+void out_rgb( struct RGB &rgb, float scale=1 ){
+    std::cout 
+    << "(" << std::fixed << std::setw(5) << std::setprecision(3) << std::setfill('0') << scale*rgb.r
+    << " " << std::fixed << std::setw(5) << std::setprecision(3) << std::setfill('0') << scale*rgb.g
+    << " " << std::fixed << std::setw(5) << std::setprecision(3) << std::setfill('0') << scale*rgb.b
+    << ") ";
+}
+
 
 
 
@@ -32,12 +63,8 @@ void parse( FILE *f );  // fwd
 void import_mtl( std::string &mtllib_path ){
     FILE *f;
     assert( f = fopen( mtllib_path.c_str(), "rb" ));
-    std::cout << "parsing " << mtllib_path << std::endl;
     parse(f);   // usiamo lo stesso parser, i tokens son cmq univoci (ci han pensato)
     fclose(f);
-    for( auto i=mtl_list.begin(); i!=mtl_list.end(); ++i ){
-        std::cout << i->first << std::endl;
-    }
 }
 
 
@@ -48,29 +75,54 @@ char *tok( char *line = 0 ){
 
 
 
+void out_raw_tri( struct MTL &mtl ){
+    // ogni token letto descrive un vertice
+    // "vert_id/uv_id/norm_id"
+    // non sappiamo in anticipop quanti vertici ci sono, ma sono minimo 3
+    // vogliamo cmq esportare triangoli
+
+    struct VUN {
+        int v,u,n;
+    };
+
+    struct VUN vun[3];
+    int i=0;
+
+    while(1){
+        char *t = tok();
+        if(!t)break;
+        assert( 3 == sscanf( t, "%d/%d/%d", &vun[i].v, &vun[i].u, &vun[i].n ));
+        if(i<2){ ++i; continue; }
+
+        out_xyz( vert_list[vun[0].v]);
+        out_xyz( vert_list[vun[1].v]);
+        out_xyz( vert_list[vun[2].v]);
+        out_rgb( mtl.diff );
+        out_rgb( mtl.emit, 1000 );
+        std::cout << std::endl;
+
+        vun[1] = vun[2];
+    }
+}
+
 
 
 void parse( FILE *f ){
 
-    std::string mtllib_path;
-    std::string obj_name;
     std::string usemtl;
 
+    // usate sono nel parsing dell'mtl
     std::string mtl_name;
     struct MTL mtl;
 
-    int face_id;
-    int vert_id;
-    int norm_id;
-    int uv_id;
 
-    bool store_mtl=false;
+    bool store_mtl = false;
 
     while(1){
         // visto che obj non è freeform
         // processiamo per linea
         char line[256];
-        if( 0 == fgets( line, sizeof(line), f ))break;
+        if(!fgets( line, sizeof(line), f ))break;
 
         // brucio i commenti
         char *c = strchr(line,'#');
@@ -87,14 +139,12 @@ void parse( FILE *f ){
             assert(c=tok());
             mtl_name = c;
             store_mtl=true;
-            std::cout << "mtl_name " << mtl_name << std::endl;
             continue;
         }
 
         if( item == "mtllib" ){
             assert(c=tok());
-            mtllib_path = c;
-            std::cout << "mtllib_path " << mtllib_path << std::endl;
+            std::string mtllib_path = c;
             import_mtl(mtllib_path);
             continue;
         }
@@ -102,7 +152,6 @@ void parse( FILE *f ){
         if( item == "usemtl" ){
             assert(c=tok());
             usemtl = c;
-            std::cout << "usemtl " << usemtl << std::endl;
             continue;
         }
 
@@ -110,7 +159,6 @@ void parse( FILE *f ){
             mtl.diff.r = atof(tok());
             mtl.diff.g = atof(tok());
             mtl.diff.b = atof(tok());
-            std::cout << "diff rgb " << mtl.diff.r << " " << mtl.diff.g << " " << mtl.diff.b << std::endl;
             continue;
         }
 
@@ -118,63 +166,45 @@ void parse( FILE *f ){
             mtl.emit.r = atof(tok());
             mtl.emit.g = atof(tok());
             mtl.emit.b = atof(tok());
-            std::cout << "emit rgb " << mtl.emit.r << " " << mtl.emit.g << " " << mtl.emit.b << std::endl;
             continue;
         }
 
         if( item == "vt" ){
-            ++uv_id;
             float u = atof(tok());
             float v = atof(tok());
-            printf("uv %d uv %f %f\n",uv_id,u,v);
             continue;
         }
 
         if( item == "vn" ){
-            ++norm_id;
             float x = atof(tok());
             float y = atof(tok());
             float z = atof(tok());
-            printf("norm %d xyz %f %f %f\n",norm_id,x,y,z);
             continue;
         }
 
         if( item == "v" ){
-            ++vert_id;
-            float x = atof(tok());
-            float y = atof(tok());
-            float z = atof(tok());
-            printf("vert %d xyz %f %f %f\n",vert_id,x,y,z);
+            struct XYZ xyz;
+            xyz.x = atof(tok());
+            xyz.y = atof(tok());
+            xyz.z = atof(tok());
+            vert_list.push_back(xyz);
             continue;
         }
 
         if( item == "o" ){
             assert(c=tok());
-            obj_name = c;
-            std::cout << "obj_name " << obj_name << std::endl;
-            face_id=0;
-            vert_id=0;
-            norm_id=0;
-            uv_id=0;
+            std::string obj_name = c;
+            // ripartiamo con una lista di vertici
+            // obj è 1-based, inseriamo un dummy vert per non far aritmetica dopo
             continue;
         }
 
         if( item == "f" ){
-            // face def
-            // f 1/1/1 2/2/1 3/3/1 4/4/1
-            ++face_id;
-            std::cout << "face " << face_id << " vtn ";
-            while(1){
-                char *t = tok();
-                if(!t)break;
-                std::string vtn = t;
-                std::cout << vtn << " ";
-            }
-            std::cout << std::endl;
+            out_raw_tri( mtl_list[usemtl]);
             continue;
         }
 
-        std::cout << "* item " << item << " skippato" << std::endl;
+//        std::cout << "* item " << item << " skippato" << std::endl;
     }
 
     if(store_mtl) mtl_list[mtl_name]=mtl;
@@ -184,7 +214,6 @@ void parse( FILE *f ){
 void import_obj( char *path ){
     FILE *f;
     assert( f = fopen( path, "rb" ));
-    std::cout << "parsing " << path << std::endl;
     parse(f);
     fclose(f);
 }
@@ -193,6 +222,10 @@ void import_obj( char *path ){
 
 int main( int argc , char *argv[] ){
     if(argc<2)return 1;
+    // nel .obj gli indici son tutti 1-based, quindi aggiungo un dummy
+    // per evitare di fare aritmetica inelegante dopo
+    struct XYZ dummy;
+    vert_list.push_back(dummy);
     import_obj(argv[1]);
     return 0;
 }
